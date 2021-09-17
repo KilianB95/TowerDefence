3862732ee1b650dc19d3ed4cf3a9c2b85707b9c8
Kilian <31303@ma-web.nl>
2021-09-17 12:40:35 +0200

    HealthBar in Progress

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+
+    public GameObject healthBarUI;
+    public Slider slider;
+
+    private void Start()
+    {
+        health = maxHealth;
+        slider.value = CalculateHealth();
+    }
+
+    private void Update()
+    {
+        slider.value = calculateHealth();
+
+        if(health < maxHealth)
+        {
+            healthBarUI.SetActive(true);
+        }
+    }
+}
